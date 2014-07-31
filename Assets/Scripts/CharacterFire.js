3a5e104622f145df33aafdc5f7a9eda14c790f74
totoro30004 <totoro30004@gmail.com>
2014-07-30 21:59:01 -0700

    Bullet ejection

--- Assets/Scripts/CharacterFire.js
+++ Assets/Scripts/CharacterFire.js
@@ -4,10 +4,12 @@ private var controller: CharacterController;
 var defaultBullet : GameObject;
 private var speed : float = 700;
 static var fireTimer : float;
+static var firing : boolean;
 
 function Start () {
 	controller = GetComponent(CharacterController);
 	fireTimer = 15;
+	firing = false;
 }
 
 function Update () {
@@ -18,11 +20,13 @@ function Fire() {
 	if (Input.GetKey(KeyCode.Space)) {
 		fireTimer --;
 		if (fireTimer <= 0) {
+			firing = true;
 			var spawn_defaultBullet = Instantiate(defaultBullet, transform.position, Quaternion.identity);
 			spawn_defaultBullet.rigidbody.AddForce(Vector3.up * speed);
 			fireTimer = 15;
 		}
 	}else if (Input.GetKeyUp(KeyCode.Space)){
 		fireTimer = 15;
+		firing = false;
 	}
 }
---
+++ Assets/Scripts/EjectShell.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+public var shell : GameObject;
+private var ejectTimer : float;
+
+
+function Start () {
+	ejectTimer = 15;
+}
+
+function Update () {
+	EjectShell();
+
+}
+
+function EjectShell () {
+	if (CharacterFire.firing == true){
+		ejectTimer --;
+		if (ejectTimer <= 0) {
+			var shell = Instantiate(shell,transform.position,Quaternion.identity);
+			shell.rigidbody.AddForce(Vector3.right * Random.Range(100, 100));
+			ejectTimer = 15;
+		}
+	}
+}
