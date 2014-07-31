c6b51832f7954e965608ef0a117dcfef083f73cc
Rowan Weismiller <rowan.weismiller@gmail.com>
2014-07-31 00:48:15 -0700

    Added cutscene functionality

---
+++ Assets/Scripts/ThreeFrameComic.js
@@ -0,0 +1,35 @@
+﻿#pragma strict
+
+var panels : GameObject[];
+private var panelMaxHeights : float[];
+
+private var currentPanel : int;
+
+private var tapRate : float = 1;
+private var nextTap : float;
+
+function Start () {
+	currentPanel = 0;
+	panelMaxHeights = [0.8, 0.5, 0.2];
+}
+
+function Update () {
+	if (Input.GetButton("Fire1") && Time.time > nextTap) {
+		MovePanel();
+	}
+}
+
+function MovePanel () {
+	nextTap = Time.time + tapRate;
+	
+	panels[currentPanel].transform.position.y = panelMaxHeights[currentPanel] - 0.3;
+	
+	while(panels[currentPanel].transform.position.y < panelMaxHeights[currentPanel]) {
+		panels[currentPanel].transform.position.y += 0.01;
+		yield WaitForSeconds(0.01);
+	}
+	
+	if (currentPanel < 2) {
+		currentPanel++;
+	}
+}
