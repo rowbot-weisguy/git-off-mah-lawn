5e3d3c8bea32b7d7840db0448bc0c59671ed7d0f
Rowan Weismiller <rowan.weismiller@gmail.com>
2014-08-06 12:18:36 -0700

    Improve the cutscene panel animations

--- Assets/Scripts/ThreeFrameComic.js
+++ Assets/Scripts/ThreeFrameComic.js
@@ -5,17 +5,22 @@ private var panelMaxHeights : float[];
 
 private var currentPanel : int;
 
-private var tapRate : float = 1;
+private var tapRate : float = 0.5;
 private var nextTap : float;
+private var speed : Vector3;
 
 function Start () {
 	currentPanel = 0;
-	panelMaxHeights = [0.8, 0.5, 0.2];
+	panelMaxHeights = [0.7, 0.5, 0.3];
 }
 
 function Update () {
 	if (Input.GetButton("Fire1") && Time.time > nextTap) {
-		MovePanel();
+		if (currentPanel < 3) {
+			MovePanel();
+		} else if (currentPanel >= 3) {
+			Debug.Log('Change scene');
+		}
 	}
 }
 
@@ -23,13 +28,13 @@ function MovePanel () {
 	nextTap = Time.time + tapRate;
 	
 	panels[currentPanel].transform.position.y = panelMaxHeights[currentPanel] - 0.3;
+	speed = new Vector3(0, 0.03);
 	
-	while(panels[currentPanel].transform.position.y < panelMaxHeights[currentPanel]) {
-		panels[currentPanel].transform.position.y += 0.01;
+	while(speed.y > 0.001) {
+		panels[currentPanel].transform.position += speed;
+		speed.y *= 0.90;
 		yield WaitForSeconds(0.01);
 	}
-	
-	if (currentPanel < 2) {
-		currentPanel++;
-	}
+
+	currentPanel++;
 }
