403c009b3107fc2dde5f7c8794765c225e8b2f73
Rowan Weismiller <rowan.weismiller@gmail.com>
2014-08-06 17:51:38 -0700

    Updated the image quality, animated the button

--- Assets/Scripts/RaccoonLosing.js
+++ Assets/Scripts/RaccoonLosing.js
@@ -1,22 +1,53 @@
 ﻿#pragma strict
 var skin : GUISkin;
-var string : String = "Damn Button";
-private var retryPosition : Rect = new Rect(50,200,100,50);
 var buttonImage : Texture2D;
+var buttonWidth : float;
+var buttonHeight : float;
+var loadLevel : String;
+
+private var buttonPos : Vector3;
+private var buttonRect : Rect;
+private var downOffset : float;
+private var speed : Vector3;
+var trigger : boolean;
 
 function Start () {
-
+	downOffset = 80;
+	speed = new Vector3(0, 10);
+	
+	buttonPos = new Vector3(
+		Screen.width/2 - buttonWidth/2,
+		Screen.height - buttonHeight + downOffset
+	);
+		
+	buttonRect = new Rect(
+		buttonPos.x,
+		buttonPos.y,
+		buttonWidth,
+		buttonHeight
+	);
 }
 
 function Update () {
-
+	if(trigger && Mathf.Abs(speed.y) >= 0.01) {
+		buttonPos -= speed;
+		speed.y *= 0.90;
+		
+		buttonRect = new Rect(
+			buttonPos.x,
+			buttonPos.y,
+			buttonWidth,
+			buttonHeight
+		);
+	}
 }
 
 function OnGUI(){
 	GUI.skin = skin;
-	
 	GUI.Label(Rect(20, 20, 1000, 500), "YOU LOSE");
-	if (GUI.Button(retryPosition, buttonImage)) {
-//		Application.LoadLevel("Still House");
+	
+	if (GUI.Button(buttonRect, buttonImage)) {
+		speed = new Vector3(0,-10);
+//		Application.LoadLevel(loadLevel);
 	}
 }
